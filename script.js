fc30f13558307cb4639c5127adaac2a6bdeacd46
dylan@dylanhulbert.com <Your-Email>
2020-02-07 20:27:45 -0800

    password criteria selection

--- script.js
+++ script.js
@@ -5,6 +5,40 @@ var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P"
 var numeric = ["1","2","3","4","5","6","7","8","9","0"]
 var special = ["!","@","#","$","%","^","&","*","(",")"]
 
+
+// this function pulls together the selected password critera
+function PasswordCriteria() {
+  var length = parseInt(prompt("length of the password: 8 - 128 characters?"));
+
+  if (length < 8) {
+    alert("must be 8 - 128 characters... try again");
+    return;
+  }
+
+  if (length > 128) {
+    alert("must be 8 - 128 characters... try again");
+    return;
+  }
+
+  var includeLowercase = confirm("lowercase?");
+  var includeUppercase = confirm("uppercase?");
+  var includeNumeric = confirm("numeric?");
+  var includeSpecial = confirm("special characters?");
+
+  if (includeLowercase === false && includeUppercase === false && includeNumeric === false && includeSpecial === false) {
+    alert("you have to pick something... try again");
+    return;
+  }
+
+  var selectedCriteria = (length, includeLowercase, includeUppercase, includeNumeric, includeSpecial);
+
+  return selectedCriteria;
+
+
+
+
+
+  
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
