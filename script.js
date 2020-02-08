6324a66f8cad8c844d23bc5dd848db4d7676b262
dylan@dylanhulbert.com <Your-Email>
2020-02-07 18:31:20 -0800

    added arrays

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+
+// arrays of lowercase, uppercase, numeric, and special characters
+var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var numeric = ["1","2","3","4","5","6","7","8","9","0"]
+var special = ["!","@","#","$","%","^","&","*","(",")"]
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
